1b60dc662a2b3b3c17d12a93382d1e8faef2f1d8
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-09-25 13:32:28 +0200

    Create person class

---
+++ person.rb
@@ -0,0 +1,19 @@
+class Person
+  attr_reader :id, :parent_permission
+  attr_accessor :name, :age
+
+  def initialize(age, parent_permission = true, name = 'Unknown')
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def is_of_age?
+    @age >= 18
+  end
+
+  def can_use_services?
+    self.is_of_age? || @parent_permission
+  end
+end
